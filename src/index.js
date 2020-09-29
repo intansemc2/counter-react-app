0470bed40c31b8d03176e8c307b1974f66d9339d
intansemc2 <intansemc2@gmail.com>
2020-09-29 21:40:25 +0700

    first react component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counterComponents';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+ReactDOM.render(<Counter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
