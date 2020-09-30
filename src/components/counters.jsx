ea62a87f922c16b52aa14b4b3e2d838b9585f823
intansemc2 <intansemc2@gmail.com>
2020-10-01 00:46:54 +0700

    single source of truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,4 +1,4 @@
-//update the state after handle events
+//single source of truth
 import React, { Component } from 'react';
 import Counter from './counter';
 
@@ -16,11 +16,32 @@ class Counters extends Component {
         this.setState({ counters: this.state.counters.filter((counter) => counter.id !== counterId) });
     };
 
+    handleReset = () => {
+        this.setState({
+            counters: this.state.counters.map((counter) => {
+                counter.value = 0;
+                return counter;
+            }),
+        });
+    };
+
+    handleIncrement = (counterId) => {
+        this.setState({
+            counters: this.state.counters.map((counter) => {
+                if (counter.id === counterId) counter.value += 1;
+                return counter;
+            }),
+        });
+    };
+
     render() {
         return (
             <div>
+                <button onClick={this.handleReset} className='btn btn-primary btn-sm m-2'>
+                    Reset
+                </button>
                 {this.state.counters.map((counter) => (
-                    <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />
+                    <Counter key={counter.id} onDelete={this.handleDelete} onReset={this.handleReset} onIncrement={this.handleIncrement} counter={counter} />
                 ))}
             </div>
         );
