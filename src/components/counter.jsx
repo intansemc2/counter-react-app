be8832694da44e299a4f8a582a2c3620d21a7c51
intansemc2 <intansemc2@gmail.com>
2020-09-30 23:40:44 +0700

    composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,3 @@
-//passing event arguments
 import React, { Component } from 'react';
 
 class Counter extends Component {
@@ -13,12 +12,12 @@ class Counter extends Component {
 
     render() {
         return (
-            <React.Fragment>
+            <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={() => this.handleIncrement({ id: 1 })} className='btn btn-secondary btn-sm'>
                     Increment
                 </button>
-            </React.Fragment>
+            </div>
         );
     }
 
