63d56f265a34190bafae4c4fcbb96eed6df2eb7a
intansemc2 <intansemc2@gmail.com>
2020-10-01 14:32:01 +0700

    update data

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     state = {
         counters: [
             { id: 1, value: 0 },
-            { id: 2, value: 4 },
-            { id: 3, value: 1 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
             { id: 4, value: 0 },
         ],
     };
--- src/index.js
+++ src/index.js
@@ -5,5 +5,4 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './App';
 
-//Destructuring Arguments
 ReactDOM.render(<App />, document.getElementById('root'));
